905e44ce0ca905a385fa11042277339652c50e31
jorgemirandaneto <jorgemirandaneto@live.com>
2019-09-17 14:29:37 -0300

    First mapping object

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize('vxrykahd', 'vxrykahd', 'BU3yb3_ZdfAcV8ZSBKlZfUiBkH0aiguO', {
+    host: 'salt.db.elephantsql.com',
+    dialect: 'postgres'
+})
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Conexão com postgres Ok!');
+    })
+    .catch(err => {
+        console.error('Erro ao conectar ao postgres:', err);
+    });

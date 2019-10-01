c4f44ad2a7fc9e251a5e5f4ac4c0ebd65369f000
jorgemirandaneto <jorgemirandaneto@live.com>
2019-10-01 13:01:12 -0300

    Finish crud item

--- models/mesuare.js
+++ models/mesuare.js
@@ -1,9 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Mesuare = sequelize.define('Mesuare', {
-    id: DataTypes.INTEGER
-  }, {});
-  Mesuare.associate = function(models) {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    description : {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {sequelize, modelName: 'Mesuare',tableName:'Mesuare' ,schema: 'stock'});
+  Mesuare.associate = function (models) {
     // associations can be defined here
   };
   return Mesuare;
